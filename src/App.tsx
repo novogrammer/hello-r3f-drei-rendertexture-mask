d30daddfb7bb453a4890931e2260b9cc7d37bd94
Yusuke Kawamoto <novogrammer@gmail.com>
2024-01-04 17:20:33 +0900

    Canvasを100lvhにする

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ function App() {
         left:0,
         top:0,
         width:"100%",
-        height:"100%",
+        height:"100lvh",
       }}>
         <Canvas eventSource={containerRef} shadows={'soft'}>
           <GlobalScene/>
